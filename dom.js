7f564a599de34e85f98bc79fb8adfcd047258121
rushi <rushikesh.tokle@gmail.com>
2025-05-19 23:02:21 +0530

    Assignment completed

--- dom.js
+++ dom.js
@@ -12,9 +12,9 @@ for(let i=0;i<img1.length;i++){
 for(let i=0;i<img1.length;i++){
     console.dir(img1[i].src);
 }
-for(let i=0;i<img1.length;i++){
-    console.dir(img1[i].src="assets/spiderman_img.png");
-}
+// for(let i=0;i<img1.length;i++){
+//     console.dir(img1[i].src="assets/spiderman_img.png");
+// }
 
 
 document.getElementsByTagName("p")[1].innerText="abc";
@@ -47,4 +47,58 @@ for(let i=0;i<changeColor.length;i++){
 
 //obj.classlist
  let check=document.querySelector(".box");
+
+
+ ///Practice Questions
+//1
+ let para=document.createElement("p");
+ para.innerText="I am Red";
+ para.style.color="red";
+ let boy=document.querySelector(".end");
+ boy.insertAdjacentElement('afterend',para);
+
+//2
+ let hthree=document.createElement("h3");
+ hthree.innerText="THIS IS HTHREE";
+ hthree.style.color="blue";
+ boy.insertAdjacentElement('beforebegin',hthree);
+
+
+ //3
+ let divi=document.createElement("div");
  
+
+ let hone=document.createElement("h1");
+ hone.innerText="I AM IN A DIV";
+ divi.append(hone);
+
+  let paregraph=document.createElement("p");
+ paregraph.innerText="MEE TOO";
+ divi.append(paregraph);
+
+ divi.classList.add("box");
+document.querySelector("body").prepend(divi);
+
+//Assignment Question 1
+let button=document.createElement("button");
+let input=document.createElement("input");
+button.innerText="Clickme";
+
+
+//asssignment 2
+button.setAttribute("id","btn");
+input.setAttribute("placeholder","username");
+
+//3
+let btn=document.querySelector("#btn");
+btn.classList.add("btnStyle");
+
+//4
+let h1=document.createElement("h1");
+h1.innerHTML="<u>DOMPractice</u>";
+document.querySelector("body").append(h1);
+
+//5
+let p=document.createElement("p");
+p.innerHTML="ApnaCollege<b>Delta</b>Practice";
+document.querySelector("body").append(p);
