a9aac0527bc48aef4cafe4ede89f0a3653a03c26
rushi <rushikesh.tokle@gmail.com>
2025-05-19 21:59:16 +0530

    Manipulating Attributes

--- dom.js
+++ dom.js
@@ -17,4 +17,34 @@ for(let i=0;i<img1.length;i++){
 }
 
 
-document.getElementsByTagName();
+document.getElementsByTagName("p")[1].innerText="abc";
+
+//QuerySelector can be used to in place of the get Element By Id and every similar thing
+console.log(document.querySelector("h1"));
+
+console.dir(document.querySelector("div a"));
+
+console.dir(document.querySelectorAll("h1"));
+
+let pare=document.querySelector("p");
+
+
+///Manipulating Attributes
+let img=document.querySelector("img");
+console.log(img);
+// img.setAttribute("id","RushiImg");
+// console.log(img.getAttribute("id"));
+
+//Manipulating Style
+
+
+let changeColor=document.querySelectorAll(".box a");
+//changeColor.style
+
+for(let i=0;i<changeColor.length;i++){
+    changeColor[i].style.color="yellow";
+}
+
+//obj.classlist
+ let check=document.querySelector(".box");
+ 
