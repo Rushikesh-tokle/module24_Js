53a66cecf5a39ebc5b9c873580147c4af65944c8
rushi <rushikesh.tokle@gmail.com>
2025-05-19 18:20:27 +0530

    getElementByTagName

--- dom.js
+++ dom.js
@@ -1,1 +1,20 @@
 console.log("DOM MANIPULATION STARTING FROM HERE");
+
+let img1=document.getElementsByClassName("oldImg");
+console.log(img1);
+let img2=document.getElementsByClassName("oldImg")[1];
+console.log(img2);
+
+for(let i=0;i<img1.length;i++){
+    console.dir(img1[i]);
+}
+
+for(let i=0;i<img1.length;i++){
+    console.dir(img1[i].src);
+}
+for(let i=0;i<img1.length;i++){
+    console.dir(img1[i].src="assets/spiderman_img.png");
+}
+
+
+document.getElementsByTagName();
